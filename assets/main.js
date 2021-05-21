5d8b62d969757b7012fb0e510626be16d1856db6
chytran <chytran@users.noreply.github.com>
2021-05-21 10:44:33 -0700

    Construciton

---
+++ assets/main.js
@@ -0,0 +1,17 @@
+const quizContainer = document.getElementById("quiz-container"); 
+const submit = document.getElementById("submit");
+const results = document.getElementById("results");
+
+const buildQuiz = () => {
+
+}
+
+const showResults = () => {
+
+}
+
+// Display quiz
+buildQuiz();
+
+// On submit
+submit.addEventListener("click", showResults);
