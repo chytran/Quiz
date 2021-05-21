4d82351bd79c8a222e75db33f25f1902200b4a74
chytran <chytran@users.noreply.github.com>
2021-05-21 11:09:37 -0700

    Progress

--- assets/main.js
+++ assets/main.js
@@ -10,6 +10,38 @@ const showResults = () => {
 
 }
 
+const questions = [
+    {
+        question: "Who wrote the Harry Potter Series?",
+        answers: {
+            a: "J.K. Rowling",
+            b: "Elon Musk",
+            c: "Carson Chaos"
+        },
+        correctAnswer: "a"
+    },
+
+    {
+        question: "What is the most popular food?",
+        answers: {
+            a: "Chicken",
+            b: "Pizza",
+            c: "Beans"
+        },
+        correctAnswer: "b"
+    },
+
+    {
+        question: "Which one of these is a Javascript package manager?",
+        answers: {
+            a: "Node.js",
+            b: "TypeScript",
+            c: "npm"
+        },
+        correctAnswer: "c"
+    },
+];
+
 // Display quiz
 buildQuiz();
 
