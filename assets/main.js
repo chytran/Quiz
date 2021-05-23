56a77e22ba5d450533547170fb367326143079c1
chytran <chytran@users.noreply.github.com>
2021-05-23 11:54:40 -0700

    Quiz set up already

--- assets/main.js
+++ assets/main.js
@@ -2,12 +2,22 @@ const quizContainer = document.getElementById("quiz-container");
 const submit = document.getElementById("submit");
 const results = document.getElementById("results");
 
+let currentQuestion = 0;
+
+const questionEl = document.getElementById("question");
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+const submitButton = document.getElementById("submit");
+
 const buildQuiz = () => {
+    questionEl.innerHTML = questions[currentQuestion].question;
 
-}
-
-const showResults = () => {
-
+    a_text.innerHTML = questions[currentQuestion].answers.a;
+    b_text.innerHTML = questions[currentQuestion].answers.b;
+    c_text.innerHTML = questions[currentQuestion].answers.c;
+    d_text.innerHTML = questions[currentQuestion].answers.d;
 }
 
 const questions = [
@@ -16,7 +26,8 @@ const questions = [
         answers: {
             a: "J.K. Rowling",
             b: "Elon Musk",
-            c: "Carson Chaos"
+            c: "Carson Chaos",
+            d: "John Cena"
         },
         correctAnswer: "a"
     },
@@ -26,7 +37,8 @@ const questions = [
         answers: {
             a: "Chicken",
             b: "Pizza",
-            c: "Beans"
+            c: "Beans",
+            d: "Cheese"
         },
         correctAnswer: "b"
     },
@@ -36,14 +48,27 @@ const questions = [
         answers: {
             a: "Node.js",
             b: "TypeScript",
-            c: "npm"
+            c: "npm",
+            d: "React.js"
         },
         correctAnswer: "c"
     },
 ];
 
+
+
 // Display quiz
 buildQuiz();
+
+submitButton.addEventListener("click", () => {
+    currentQuestion++;
+
+    if (currentQuestions === 2) {
+
+    }
+
+    buildQuiz();
+});
 
 // On submit
 submit.addEventListener("click", showResults);
